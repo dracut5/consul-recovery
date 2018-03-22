5c7efddc49a4cef4ef999e58b78167316a5abed0
Artem <matviienko.artem@pdffiller.team>
2018-03-22 18:00:58 +0200

    lambda realated fix

--- consul-snap.py
+++ consul-snap.py
@@ -6,13 +6,14 @@ from datetime import datetime
 import os
 import boto3
 
-consulAddrPort = os.environ['CONSUL_ADDRESS'] if 'CONSUL_ADDRESS' in os.environ.keys() else "127.0.0.1:8500"
-consulSnapUrl = 'http://{}/v1/snapshot'.format(consulAddrPort)
-bucketS3 = 'terraform-signnow-dev-remote-states'
-KeyS3 = 'backup/consul-cl01/'+ datetime.strftime(datetime.now(), "%Y-%m-%d_%H-%M-%S") + '_snapshot.tar.gz'
-
 
 def mainFunc(json_input, context):
+
+    consulAddrPort = os.environ['CONSUL_ADDRESS'] if 'CONSUL_ADDRESS' in os.environ.keys() else "127.0.0.1:8500"
+    consulSnapUrl = 'http://{}/v1/snapshot'.format(consulAddrPort)
+    bucketS3 = 'terraform-signnow-dev-remote-states'
+    KeyS3 = 'backup/consul-cl01/'+ datetime.strftime(datetime.now(), "%Y-%m-%d_%H-%M-%S") + '_snapshot.tar.gz'
+    
     response = urlopen(consulSnapUrl)
     snapArchive = response.read()
 ###Use env vars for AWS credentials 
