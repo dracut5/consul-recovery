395b8dbcb2f98c2798224254033a01a3fc64ecad
dracut <matviienko.artem@pdffiller.team>
2018-03-30 18:08:54 +0300

    new version

--- consul-snap.py
+++ consul-snap.py
@@ -9,14 +9,15 @@ import boto3
 
 def mainFunc(event, context):
 
-    consulAddrPort = os.environ['CONSUL_ADDRESS'] if 'CONSUL_ADDRESS' in os.environ.keys() else "127.0.0.1:8500"
+    consulAddrPort = os.environ['CONSUL_ADDRESS']
+    bucketS3 = os.environ['BUCKET']
+    bucketS3path = os.environ['BUCKET_PATH'] if 'BUCKET_PATH' in os.environ.keys() else ''
     consulSnapUrl = 'http://{}/v1/snapshot'.format(consulAddrPort)
-    bucketS3 = 'terraform-signnow-dev-remote-states'
-    KeyS3 = 'backup/consul-cl01/'+ datetime.strftime(datetime.now(), "%Y-%m-%d_%H-%M-%S") + '_snapshot.tar.gz'
-    
+    KeyS3 = bucketS3path + datetime.strftime(datetime.now(), "%Y-%m-%d_%H-%M-%S") + '_snapshot.tar.gz'
+
     response = urlopen(consulSnapUrl)
     snapArchive = response.read()
-###Use env vars for AWS credentials 
+
     s3 = boto3.resource('s3')
     s3.Bucket(bucketS3).put_object(Key=KeyS3, Body=snapArchive)
 
