949f1d68f79b26a3f4886769cdd3cb31e7b4a2f0
dracut <matviienko.artem@pdffiller.team>
2018-04-02 11:01:24 +0300

    add remove func

--- consul-snap.py
+++ consul-snap.py
@@ -7,31 +7,29 @@ import os
 import boto3
 
 
-def saveCopies():
-    keys=[]
+def removeCopies(keepCopies, s3, bucketS3, prefix):
+    
+    files = []
+    for obj in s3.Bucket(bucketS3).objects.filter(Prefix=prefix):
+       files.append(obj.key)
 
-    consulAddrPort = os.environ['CONSUL_ADDRESS']
-    bucketS3 = os.environ['BUCKET']
-    bucketS3path = os.environ['BUCKET_PATH'] if 'BUCKET_PATH' in os.environ.keys() else ''
-    n = 10
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucketS3)
-    for obj in bucket.objects.filter(Prefix='test/'):
-       keys.append(obj.key)
-    #print(keys)
-    keys.sort()
-    keys.pop(0)
-    print("Sorted")
-    print(keys) 
-    s3.Object(bucketS3, keys[0]).delete()
-      
-   
+    files.sort()
+    if files[0] == prefix: 
+        files.pop(0)
+    copies = len(files)
+    
+    while copies > keepCopies:
+        delFile = files.pop(0)
+        s3.Object(bucketS3, delFile).delete()
+        copies -= 1
 
 def mainFunc(event, context):
 
     consulAddrPort = os.environ['CONSUL_ADDRESS']
     bucketS3 = os.environ['BUCKET']
     bucketS3path = os.environ['BUCKET_PATH'] if 'BUCKET_PATH' in os.environ.keys() else ''
+    keepCopies = os.environ['COPIES'] if 'COPIES' in os.environ.keys() else 10   
+
     consulSnapUrl = 'http://{}/v1/snapshot'.format(consulAddrPort)
     KeyS3 = bucketS3path + datetime.strftime(datetime.now(), "%Y-%m-%d_%H-%M-%S") + '_snapshot.tar.gz'
 
@@ -41,11 +39,13 @@ def mainFunc(event, context):
     s3 = boto3.resource('s3')
     s3.Bucket(bucketS3).put_object(Key=KeyS3, Body=snapArchive)
 
+    removeCopies(keepCopies, s3, bucketS3, bucketS3path)
+
+
     return bucketS3 + '/' + KeyS3
 
 
 
 if __name__ == '__main__':
-    #result = mainFunc('input', 'context')
-    #print(result) 
-    saveCopies()
+    result = mainFunc('input', 'context')
+    print(result) 
