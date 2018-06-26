daf117ce1a98c53da5fdbbf671b9cf213c6444f9
dracut <matviienko.artem@pdffiller.team>
2018-06-26 16:21:17 +0300

    snapshot func

--- es-snap.py
+++ es-snap.py
@@ -5,22 +5,59 @@ from datetime import datetime
 import requests
 import os
 import boto3
+import json
+
+def rotateRepo(elasticAddrPort, month, year):
+ 
+    yearString = str(year)
+
+    if month in range(1,4):
+        repo = 'first_quarter_' + yearString
+    elif month in range(4,7):
+        repo = 'second_quarter_' + yearString
+    elif month in range(7,10):
+        repo = 'third_quarter_' + yearString
+    elif date.month in range(10,13):
+        repo = 'fourth_quarter_' + yearString
+    
+    repoInfo = requests.get('http://{}/_cat/repositories'.format(elasticAddrPort))
+    repoList = []
+    for r in repoInfo.text.splitlines():
+        repoList.append(r.split()[0])
+
+    if repo not in repoList:
+        repoUrl = 'http://{}/_snapshot/{}'.format(elasticAddrPort,repo)
+        repoData = {
+            "type": "fs",
+            "settings": {
+                "location": repo
+                }
+            }
+        requests.put(repoUrl, json=repoData)
+        print("New repo {} was created.".format(repo))
+    else:
+        print("Repo exists, continue...")
+     
+    return repo 
 
 
 def createSnap():
-    elasticAddrPort = os.environ['ES_ADDRESS']
-    elasticRepo = os.environ['ES_REPO']
-    testURL = 'http://{}/_cluster/health?pretty=true'.format(elasticAddrPort)
-    snapUrl = 'http://{}/_snapshot/{}/{}_snapshot'.format(elasticAddrPort,elasticRepo,datetime.strftime(datetime.now(), "%Y-%m-%d"))
+   
+    elasticAddrPort = os.environ['ES_ADDRESS'] if 'ES_ADDRESS' in os.environ.keys() else 'localhost:9200'
+    date = datetime.now()
+
+    repo = rotateRepo(elasticAddrPort, date.month, date.year)
+
+    snapUrl = 'http://{}/_snapshot/{}/snapshot_{}'.format(elasticAddrPort,repo,datetime.strftime(date, "%Y.%m.%d"))
     
-    snapParams = {
+    data = {
         "ignore_unavailable": "true",
         "partial": "true",
         "include_global_state": "false"
         }
 
-    r = requests.get(testURL)
-    return r.text
+    r3 = requests.put(snapUrl, json=data)
+    return r3.json()
 
 
 if __name__ == '__main__':
