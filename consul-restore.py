b95eb27db8f01a172c5e5b1beef9d66809f8cd2e
dracut <matviienko.artem@pdffiller.team>
2018-07-02 17:15:16 +0300

    working version of consul restore script

--- consul-restore.py
+++ consul-restore.py
@@ -1,18 +1,51 @@
 #!/usr/bin/env python3
 
 
-from datetime import datetime
 import requests
 import argparse
+import sys
+import os
 import boto3
 
+try:
+    action = sys.argv.pop(1)
+except IndexError:
+    print("First argument shoulb be 'list' or 'restore'.")
+    exit()
+
+def getS3bucket(profile, bucket):
+    session = boto3.Session(profile_name=profile)
+    s3 = session.resource('s3')
+    backupBucket = s3.Bucket(bucket)
+
+    return backupBucket
+
+def restore(host, bucket, snapshot, profile):
+    tmpFile = "/tmp/consul_snapshot.tar.gz"
+    bucket = getS3bucket(profile, bucket)
+    consulRestoreUrl =  'http://{}/v1/snapshot'.format(host)
+    with open(tmpFile, 'wb') as data:
+        bucket.download_fileobj(snapshot, data)
+    with open(tmpFile, 'rb') as data:
+        requests.put(consulRestoreUrl, data=data.read())
+    os.remove(tmpFile)
+ 
+def s3List(bucket, bucketPath, profile):
+    bucket = getS3bucket(profile, bucket)
+    for object in bucket.objects.filter(Prefix=bucketPath):
+        print(object)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--host",required=True)
-parser.add_argument("--snapshot",required=True)
-args = parser.parse_args()
+parser.add_argument("--bucket")
+parser.add_argument("--profile")
 
-consulRestoreUrl =  'http://{}/v1/snapshot'.format(args.host)
-snap = open(args.snapshot, 'rb')
-requests.put(consulRestoreUrl,data=snap.read())
-snap.close()
+if action == "list":
+    parser.add_argument("--bucket_path")
+    args = parser.parse_args()
+    s3List(args.bucket, args.bucket_path, args.profile)
+elif action == "restore":
+    parser.add_argument("--consul_address",required=True)
+    parser.add_argument("--snapshot",required=True)
+    args = parser.parse_args()
+    restore(args.consul_address, args.bucket, args.snapshot, args.profile)
+
